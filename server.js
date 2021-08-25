5424c0613cee7fcc081d20482ca9ecc8d352cf51
MAYANK AGARWAL <mayankagarwal.cs19@rvce.edu.in>
2021-08-26 01:37:19 +0530

    Cart, Login and Register Pages

--- server.js
+++ server.js
@@ -8,14 +8,30 @@ const PORT = process.env.PORT || 3000;
 // Assets
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
 // Set Template Engine
 app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views"));
 app.set("view engine", "ejs");
+
+// Home Page
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+// Cart Page
+app.get("/cart", (req, res) => {
+  res.render("customers/cart");
+});
+
+// Login Page
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+// Regiter Page
+app.get("/register", (req, res) => {
+  res.render("auth/register");
+});
 
 app.listen(3000, () => {
   console.log(`Listening on port ${PORT}`);
